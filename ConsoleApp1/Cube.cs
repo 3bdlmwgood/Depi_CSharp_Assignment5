08abcefb735538485bf61586b7ccb287e807c8f1
3bdlmwgood <abdullahmohamedabdelmawgood@gmail.com>
2026-09-18 17:00:07 +0300

    Q2

---
+++ ConsoleApp1/Cube.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Cube : Shape
+    {
+        public double Depth { get; set; }
+
+        public Cube(double width,double height,double depth) : base(width,height)
+        {
+            Depth = depth;
+        }
+
+        public new double Area()
+        {
+            return base.Area() * Depth; 
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Width = {Width} , Height = {Height} , Depth = {Depth}");
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,9 +9,22 @@
             Shape shape = new Shape(3,5);
 
             Console.WriteLine(shape);
-            Console.WriteLine($"Area= {shape.Area()}");
+            Console.WriteLine($"Area = {shape.Area()}");
 
             #endregion
+
+
+            #region Q2
+
+            Cube cube = new Cube(2,3,4);
+
+            cube.Print();
+            Console.WriteLine($"Area =  {cube.Area()}");
+
+            #endregion
+
+
+
         }
     }
 }
